ed31f63231f669481e8a54b4ce9e001a73343350
enommmm <50266464+enommmm@users.noreply.github.com>
2021-01-24 10:29:27 +0900

    initial content
    練習です。

---
+++ practice/js/my_script.js
@@ -0,0 +1,42 @@
+// ドロワーメニュー
+$(function () {
+    $("#acMenu dt").on("click", function () {
+      $(this).next().slideToggle();
+    });
+  });
+
+
+// ページトップボタン
+$ (function(){
+    // 変数宣言と代入
+    let pageTop =$('#page-top');
+    // ボタン非表示
+    pageTop.hide();
+    // 80pxスクロールしたらボタン表示
+    $ (window).scroll(function () {
+      if ($(this).scrollTop() > 80) {
+        // 0.3秒でフェードイン
+        pageTop.fadeIn(300);
+      } else {
+        // 0.3秒でフェードアウト
+        pageTop.fadeOut(300);
+      }
+    });
+    // 0.5秒でページトップへ戻る
+    pageTop.click(function () {
+      $('body, html').animate({ scrollTop: 0}, 500);
+      return false;
+    })
+  })
+
+
+  /*モーダルで拡大表示*/
+$(function () {
+    $(".course-item img").click(function () {
+      $("#gray-display").html($(this).prop('outerHTML'));
+      $("#gray-display").fadeIn(200);
+    });
+    $("#gray-display, #gray-display img").click(function () {
+      $("#gray-display").fadeOut(200);
+    });
+  });
